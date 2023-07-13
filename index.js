3c6d3f10d54e0e8359e21fc6b31ebead2eecd54d
HarshaVardhan <harshavardhan.n@atlan.com>
2023-07-13 16:10:16 +0530

    feat: add maxRank to determine depths

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ module.exports = {
   debug: require("./lib/debug"),
   util: {
     time: require("./lib/util").time,
-    notime: require("./lib/util").notime
+    notime: require("./lib/util").notime,
+    maxRank: require("./lib/util").maxRank,
   },
-  version: require("./lib/version")
+  version: require("./lib/version"),
 };
